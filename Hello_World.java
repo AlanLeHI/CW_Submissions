a1d362de829f97d7b5abb621f138aab409148df3
AlanLeHI <toastpyy@gmail.com>
2024-08-14 10:28:59 -1000

    Create Hello_World.java

---
+++ Hello_World.java
@@ -0,0 +1,15 @@
+/*This code is for the first assignment. This does two things, it prints Hello World and then I'll submit it
+ */
+public class Hello_World {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Task 1: Print out "Hello World"
+		System.out.println("Hello World");
+		//Task 2: Print out Hello World on another line and again on another line
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+	}
+	
+
+}
