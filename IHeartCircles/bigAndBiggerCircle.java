71c0965eb3ade1ba906e7593f78bfc69b4ffa750
AlanLeHI <toastpyy@gmail.com>
2024-12-04 08:28:42 -1000

    Update bigAndBiggerCircle.java

--- IHeartCircles/bigAndBiggerCircle.java
+++ IHeartCircles/bigAndBiggerCircle.java
@@ -1,8 +1,35 @@
 import java.util.Scanner;
 public class Main {
     static Scanner myReader = new Scanner(System.in);
+
+    repeatTillStop();
+    
     public static void main(String[] args) {
         firstCircle();
+    }
+
+    /*
+repeatidly asks the stupid user for strings til they stay stop and keeps track of 
+how many horrible messages they've said
+ */
+    public static void repeatTillStop()
+    {
+        //gets the User's text
+        String text = myReader.nextLine();
+        //blank until User changes it inside the loop
+        String stopper = "";
+        //to keep track of the # of messages
+        int i = 0;
+        //repeatidly ask for messages till they type stop
+        while (!(stopper.equalsIgnoreCase("stop")))
+        {
+            //adds 1 to counter
+            i++;
+            //prints their message
+            System.out.println(i + ". " + text);
+            //changes the variable stopper into their text
+            stopper = myReader.nextLine();
+        }
     }
     
     /*
