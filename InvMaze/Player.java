5322f66c6c550ceca22ae1434a0f24d589f54f5c
AlanLeHI <toastpyy@gmail.com>
2025-04-15 23:26:30 -1000

    Update Player.java

--- InvMaze/Player.java
+++ InvMaze/Player.java
@@ -3,7 +3,13 @@ public class Player {
 private int rPos;
 private int cPos;
 
+/**
+ * has all the player stats
+ */
 
+/**
+ * Player constructor, holds their stats
+ */
 public Player()
 {
 	rPos = 0;
@@ -11,24 +17,45 @@ public Player()
 }
 
 
+/**
+ * returns row position
+ * @return
+ */
 public int getrPos() {
 	return rPos;
 }
 
-
+/**
+ * sets row position
+ * @param rPos
+ */
 public void setrPos(int rPos) {
 	this.rPos = rPos;
 }
 
-
+/**
+ * gets col position
+ * @return
+ */
 public int getcPos() {
 	return cPos;
 }
 
-
+/**
+ * sets col position
+ * @param cPos
+ */
 public void setcPos(int cPos) {
 	this.cPos = cPos;
 }
+
+/**
+ * moves the guy and reveals the path before
+ * @param guy
+ * @param maze
+ * @param rPos
+ * @param cPos
+ */
 public void reposition(Player guy, GameTile[][] maze, int rPos, int cPos)
 {
 	maze[guy.getrPos()][guy.getcPos()].setHasPlayer(false);
