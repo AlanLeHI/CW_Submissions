eaeae775e5839c7ab6e2e7c947cf88846bc9fb24
AlanLeHI <toastpyy@gmail.com>
2025-04-21 23:55:06 -1000

    Update Recursion1.java

--- CodingBatSolns/Recursion1.java
+++ CodingBatSolns/Recursion1.java
@@ -10,3 +10,14 @@ public int fibonacci(int n) {
   }
   return fibonacci(n-1) + fibonacci(n-2);
 }
+
+//factorial
+public int factorial(int n) {
+  if (n == 0)
+  {
+    return 1;
+  }
+  return n * factorial(n-1);
+}
+
+//
