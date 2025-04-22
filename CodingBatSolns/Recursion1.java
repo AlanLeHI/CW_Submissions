8aee140436819fbcb143d5f502414880c9245115
AlanLeHI <toastpyy@gmail.com>
2025-04-21 23:57:59 -1000

    Update Recursion1.java

--- CodingBatSolns/Recursion1.java
+++ CodingBatSolns/Recursion1.java
@@ -20,4 +20,12 @@ public int factorial(int n) {
   return n * factorial(n-1);
 }
 
-//
+//bunnyEars
+public int bunnyEars(int bunnies) {
+  if(bunnies == 0)
+  {
+    return 0;
+  }
+  return 2 + (bunnyEars(bunnies - 1));
+}
+
