8677667f22034213fc23f2b2279f02cda4bb61ea
AlanLeHI <toastpyy@gmail.com>
2025-04-24 12:12:16 -1000

    Update Isorteda2darray.java

--- Isorteda2darray.java
+++ Isorteda2darray.java
@@ -69,6 +69,9 @@ public static double[][] sortUD(double [][] toBeSorted)
 	return toBeSorted;
 }
 
+/**
+* the b(est) (s)ort(er), sorts an arraylist
+*/
 public static void bort(ArrayList<Double> numArr)
 {
 	
